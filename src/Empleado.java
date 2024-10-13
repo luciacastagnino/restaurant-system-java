e3ce45fa5bbcd69bc31e1bc21f0763ffd27317c1
Brenda2310 <brendafernandez2310@gmail.com>
2024-10-13 15:05:12 -0300

    cambio horasExtra

--- src/Empleado.java
+++ src/Empleado.java
@@ -11,12 +11,12 @@
  */
 public abstract class Empleado extends Persona {
     protected double sueldo;
-    protected int horasExtra;
+    //protected int horasExtra;
 
-    public Empleado(String nombre, String apellido, String dni, String telefono, String direccion, String email, double sueldo, int horasExtra) {
+    public Empleado(String nombre, String apellido, String dni, String telefono, String direccion, String email, double sueldo/*, int horasExtra*/) {
         super(nombre, apellido, dni, telefono, direccion, email);
         this.sueldo = sueldo;
-        this.horasExtra = horasExtra;
+        //this.horasExtra = horasExtra;
     }
 
     ///Getters y Setters
@@ -28,11 +28,14 @@ public abstract class Empleado extends Persona {
         this.sueldo = sueldo;
     }
 
-    public int getHorasExtra() {
+   /* public int getHorasExtra() {
         return horasExtra;
     }
 
     public void setHorasExtra(int horasExtra) {
         this.horasExtra = horasExtra;
     }
+
+    */
+
 }
--- src/EmpleadoMedioTiempo.java
+++ src/EmpleadoMedioTiempo.java
@@ -12,8 +12,8 @@
 public class EmpleadoMedioTiempo extends Empleado{
     private int horasTrabajadas;
 
-    public EmpleadoMedioTiempo(String nombre, String apellido, String dni, String telefono, String direccion, String email, double sueldo, int horasExtra, int horasTrabajadas) {
-        super(nombre, apellido, dni, telefono, direccion, email, sueldo, horasExtra);
+    public EmpleadoMedioTiempo(String nombre, String apellido, String dni, String telefono, String direccion, String email, double sueldo, /*int horasExtra,*/ int horasTrabajadas) {
+        super(nombre, apellido, dni, telefono, direccion, email, sueldo /*,horasExtra*/);
         this.horasTrabajadas = horasTrabajadas;
     }
 

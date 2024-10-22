46cba7330dc91a2c01dc52e6a50a88677f72f5d5
Brenda2310 <brendafernandez2310@gmail.com>
2024-10-22 14:38:41 -0300

    usuario

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Usuario<T> {
     private String userName;
     private String contrasenia;
     private T user;
+
+    public static void LogIn () {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Bienvenido! Ingrese sus datos:");
+
+        System.out.println("Nombre de usuario: ");
+        String usuario = scanner.nextLine();
+
+        System.out.println("Contraseña: ");
+        String contraseña = scanner.nextLine();
+
+
+
+    }
 }

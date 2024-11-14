5aa4ef2b83ad5ee8087f7073fba1ad312d60a77d
Melina Di Meglio <meli.dime@gmail.com>
2024-11-14 18:26:10 -0300

    clase

--- src/Plato.java
+++ src/Plato.java
@@ -92,8 +92,6 @@ public class Plato {
                 ", precio=" + precio +
                 ", disponibilidad=" + disponibilidad +
                 '}';
-
-
-        ///comentario
+        
     }
 }

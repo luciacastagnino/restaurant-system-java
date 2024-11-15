7bbe735ba151b7343d8577d174964333b9f0e047
Melina Di Meglio <meli.dime@gmail.com>
2024-11-14 22:19:19 -0300

    documentacion

--- src/TipoPlato.java
+++ src/TipoPlato.java
@@ -1,3 +1,18 @@
+/**
+ * El enum TipoPlato representa los diferentes tipos de platos disponibles en el menú del restaurante.
+ * Los tipos de platos incluyen:
+ * - DESAYUNO: Plato destinado para el desayuno.
+ * - BRUNCH: Plato para el brunch, una combinación entre desayuno y almuerzo.
+ * - ALMUERZO: Plato destinado para el almuerzo.
+ * - CENA: Plato destinado para la cena.
+ * - POSTRE: Plato para el postre.
+ * - BEBIDA: Bebidas disponibles en el restaurante.
+ * - ENTRADAS: Platos iniciales servidos antes del plato principal.
+ *
+ * @author Melina
+ * @since 2024
+ * @version 1
+ */
 public enum TipoPlato {
 
     DESAYUNO, BRUNCH, ALMUERZO, CENA, POSTRE, BEBIDA, ENTRADAS;

35b9f90895b6a2489cf99ff2b606f26667afea87
Melina Di Meglio <meli.dime@gmail.com>
2024-11-14 22:18:16 -0300

    documentacion

--- src/TipoPago.java
+++ src/TipoPago.java
@@ -1,3 +1,15 @@
+/**
+ * El enum TipoPago representa los diferentes métodos de pago disponibles en el restaurante.
+ * Los métodos de pago incluyen:
+ * - EFECTIVO: Pago realizado con dinero en efectivo.
+ * - CREDITO: Pago realizado mediante tarjeta de crédito.
+ * - DEBITO: Pago realizado mediante tarjeta de débito.
+ *
+ * @author Melina
+ * @since 2024
+ * @version 1
+ */
+
 public enum TipoPago {
 
     EFECTIVO, CREDITO, DEBITO;

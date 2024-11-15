0105fdcee80e2a95be04d0cccc3b2e1e0d37d04b
Melina Di Meglio <meli.dime@gmail.com>
2024-11-14 22:17:19 -0300

    documentacion

--- src/TipoCliente.java
+++ src/TipoCliente.java
@@ -1,3 +1,14 @@
+/**
+ * El enum TipoCliente representa los diferentes tipos de clientes en el restaurante.
+ * Los tipos de clientes incluyen:
+ * - ESTANDAR: Clientes regulares del restaurante.
+ * - PREMIUM: Clientes que disfrutan de beneficios adicionales debido a su nivel de membresía.
+ * - VIP: Clientes con el nivel más alto de beneficios y exclusividad.
+ *
+ * @author Melina
+ * @since 2024
+ * @version 1
+ */
 public enum TipoCliente {
     ESTANDAR, PREMIUM,  VIP;
 }

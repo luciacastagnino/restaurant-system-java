6939e4a54f96b4d77a3d5aefe55cb20bf1bd8b1c
Brenda2310 <brendafernandez2310@gmail.com>
2024-11-16 14:28:01 -0300

    TICKET INCOMPLETO

--- src/Gestion/GestionTickets.java
+++ src/Gestion/GestionTickets.java
@@ -1,4 +1,53 @@
 package Gestion;
 
-public class GestionTickets {
+public class GestionTickets implements MetodosBasicosGestion{
+    @Override
+    public void ingresarUsuario() {
+
+    }
+
+    @Override
+    public void mostrarDatosUsuario(Object o) {
+
+    }
+
+    @Override
+    public Object modificarUsuario(Object o) {
+        return null;
+    }
+
+    @Override
+    public void agregarYguardar(Object o) {
+
+    }
+
+    @Override
+    public void darDeBajaUsuario(Object o) {
+
+    }
+
+    @Override
+    public void mostrarColeccion() {
+
+    }
+
+    @Override
+    public Object encontrarUsuario(String dni) {
+        return null;
+    }
+
+    @Override
+    public Object encontrarUsuario(int id) {
+        return null;
+    }
+
+    @Override
+    public void listarUsuarios(String nombre) {
+
+    }
+
+    @Override
+    public void listarUsuarios(boolean aux) {
+
+    }
 }

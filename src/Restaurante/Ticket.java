3940f759ef5ed507c95c40ac37e2bcad9e1ff1a3
Brenda2310 <brendafernandez2310@gmail.com>
2024-11-16 20:20:41 -0300

    cambio

--- src/Restaurante/Ticket.java
+++ src/Restaurante/Ticket.java
@@ -30,7 +30,8 @@ public class Ticket {
     private MenuRestaurante menuRestaurante;
     private Scanner scanner;
 
-    public Ticket (int id, Reserva reserva, Empleado empleado, LocalDateTime horaEmision, List<Plato> platos, Cliente cliente double precio, TipoPago tipoPago, double propina) {
+    public Ticket (int id, Reserva reserva, Empleado empleado, LocalDateTime horaEmision, List<Plato> platos,
+                   Cliente cliente, double precio, TipoPago tipoPago, double propina) {
         this.id = id;
         this.reserva = reserva;
         this.empleado = empleado;

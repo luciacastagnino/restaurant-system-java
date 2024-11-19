c047862732f07069f7083f2a8e9ece224b5a3253
Brenda2310 <brendafernandez2310@gmail.com>
2024-11-19 18:54:51 -0300

    mesayareservada DOC

--- src/Restaurante/MesaYaReservadaException.java
+++ src/Restaurante/MesaYaReservadaException.java
@@ -1,5 +1,13 @@
 package Restaurante;
 
+/**
+ * Exception lanzada al ingresar una mesa que ya se encuentra reservada en ese mismo dia a la misma hora
+ * Casos de uso: Al ingresar nuevas reservas o modificarlas
+ *
+ * @author Brenda
+ * @since 2024
+ * @version 1
+ */
 public class MesaYaReservadaException extends Exception{
      public MesaYaReservadaException(String message) {
             super(message);
